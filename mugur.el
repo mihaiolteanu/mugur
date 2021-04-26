0316d59c656a9ed177c16b9aa249a2f0315df0e3
Mihai Olteanu <mihai_olteanu@fastmail.fm>
2021-04-26 10:08:08 +0300

    Update Commentary section

--- mugur.el
+++ mugur.el
@@ -23,49 +23,23 @@
 
 ;;; Commentary:
 
-;; Generate all the keymaps, enums, arrays, make files and everything needed for
-;; building a hex file and flashing a qmk powered keyboard.  See the github
-;; README for details and usage.
+;; Mugur is a keyboard configurator that supports all QMK compatible keyboards.
+;; mugur-mugur accepts a keymap (a list of all the keyboard layers and keys),
+;; and it generates the equivalent QMK C code (keymap.c, config.h and rules.mk).
+;; Currently supported features include all the basic QMK keycodes, the mod-tap
+;; and modifiers functionalities, one-shot keys, layer toggle, macros, tap
+;; dance, leader key and combos.  Additionally, an Emacs keybound function can
+;; also be specified as a valid key.
 
-;;; Definitions used in this package:
-;; * mugur-key - The most basic entry in the mugur-keymap.  It represents the
-;; list of all valid and mugur specific symbols, strings, characters and lists
-;; that can be used by the user in building its mugur-keymap.  It is interpreted
-;; by the mugur package and used in the end to generate the qmk-keymap.  There
-;; need not be a one-to-one correspondence with a mugur-key and a qmk-keycode,
-;; as it happens in the case of macros, for example (a single mugur-key is used
-;; to fill multiple places in the keymap.c, for example)
-
-;; * mugur-modifier - One of the symbols C, M, G or S which stand for Ctrl, Alt,
-;; Win and Shift, respectively and which correspond to the qmk-modifier KC_LCTL,
-;; KC_LALT, KC_LGUI and KC_LSHIFT respectively.  A mugur-modifier is a
-;; restricted list of mugur-keys.
-
-;; * qmk-modifier - One of the KC_LCTL, KC_LALT, KC_LGUI and KC_LSHIFT
-;; qmk-keycodes, respectively, which stand for Ctrl, Alt, Win and Shift,
-;; respectively.
-
-;; * qmk-raw-modifier - Similar to qmk-modifier, but without the KC_ part, that
-;; is LCTL, LALT, LGUI and LSHIFT
-
-;; * qmk-keycode - The qmk equivalent code for the mugur-key given by the user.
-;; Thus, a symbol like 'a, given in the mugur-keymap is transformed into the
-;; equivalent qmk-keycode "KC_A".  These are the codes that are written to the
-;; qmk-matrix
-
-;; * qmk-layer - The totality of all qmk-keycode's, toghether with a layer
-;; code/name that is part of a qmk-matrix.
-
-;; * qmk-matrix - The matrix found in the qmk_firmware's file keymap.c.  It
-;; contains all the qmk-keycode's and all the qmk-layer's, as specified by the
-;; qmk documentation.  This is what is actually used to decide which keys does
-;; what on your keyboard
-
-;; * mugur-layer - The totality of all the mugur-key's, together with a string
-;; representing the layer name, that is used to generate the qmk-layer.
-
-;; * mugur-keymap - The totally of all the mugur-layer's.  This is the mugur
-;; equivalent for the qmk-matrix.
+;; Throught this package, mugur-key refers to any of the valid mugur specific
+;; symbols, strings, characters or lists that can be used in building the
+;; keymap.  It is roughly equivalent to an QMK keycode, as given in the keymaps
+;; matrix in config.c.  To differentiate between the user-side of things and the
+;; qmk-side, I'm using similar terms throught the package, like mugur-keymap and
+;; qmk-keymap, for example, where by the first term is to be understood the
+;; keymap as given by the user, containing mugur specific entries, and by the
+;; second the transformed keymap, containing QMK keycodes, ready to be written
+;; to the generated files.  The same applies to the other terms.
 
 ;;; Code:
 
